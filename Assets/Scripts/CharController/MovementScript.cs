8f2719ae0852f23d1493d9526fb132d58a78372e
riktothepast <rik@502studios.net>
2016-03-08 21:01:32 -0600

    adds "VR" test scene.

---
+++ Assets/Scripts/CharController/MovementScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour
+{
+    Vector3 velocity;
+    public float speedFactor;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    CharacterController controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            moveDirection = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
+            moveDirection *= speedFactor;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
